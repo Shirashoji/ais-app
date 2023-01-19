8a3dfe354580ef37290b1c57aa03dd2c7683a5eb
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2023-01-19 23:09:57 +0900

    commit

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
-import { FC, useEffect, useState } from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import Stack from '@mui/material/Stack';
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -37,34 +34,45 @@ const storageRef = ref(storage, `gs://${import.meta.env.VITE_FIREBASE_STORE_BUCK
 export default function UploadButtons() {
     const [image, setImage] = useState(null);
     const changeUploadImage = async (event) => {
+        console.log(event.target.files[0]);
+        console.log(event.target.files[0].name);
         const { name, files } = event.target;
         setImage(files[0]);
-        console.log(image);
         try {
-            const imageRef = ref(storage, `gs://${import.meta.env.VITE_FIREBASE_STORE_BUCKET}/images/${image.name}`);
+            const imageRef = ref(storage, `gs://${import.meta.env.VITE_FIREBASE_STORE_BUCKET}/images/${files[0]}`);
             uploadBytes(imageRef, image).then(() => {
                 console.log("Uploaded a file!");
             });
         } catch (err) {
             console.log(err);
         }
+    };
+    if (image == null) {
+        return (
+            <div>
+                <Box sx={{ p: 10, border: 'dashed grey' }}>
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                        <Button variant="contained" component="label">
+                            Upload
+                            <input hidden accept="image/*" multiple type="file" onChange={changeUploadImage} />
+                        </Button>
+                    </Stack>
+                </Box>
+            </div>
+        );
+    } else {
+        setTimeout(() => {
+            window.location.href = "/Gallery";
+        }, 3 * 1000);
+        return (
+            <Box sx={{ width: '100%', maxWidth: 500 }}>
+                <Typography variant="h4" gutterBottom>
+                    Galleryに移動します．
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    画像は適切にアップロードされました．
+                </Typography>
+            </Box >
+        );
     }
-    return (
-        <div>
-            {(() => {
-                if (image) {
-                    <img src={URL.createObjectURL(image)} alt="upload" />
-                }
-            })()}
-
-            <Box sx={{ p: 10, border: 'dashed grey' }}>
-                <Stack direction="row" alignItems="center" spacing={2}>
-                    <Button variant="contained" component="label">
-                        Upload
-                        <input hidden accept="image/*" multiple type="file" onChange={changeUploadImage} />
-                    </Button>
-                </Stack>
-            </Box>
-        </div>
-    );
 }
