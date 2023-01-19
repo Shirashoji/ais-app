a7934d5ae6d7d28d56dc0a9d16f42ed9965e7279
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2023-01-19 23:25:05 +0900

    fix bug upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -32,47 +32,32 @@ const storageRef = ref(storage, `gs://${import.meta.env.VITE_FIREBASE_STORE_BUCK
 
 
 export default function UploadButtons() {
-    const [image, setImage] = useState(null);
-    const changeUploadImage = async (event) => {
-        console.log(event.target.files[0]);
-        console.log(event.target.files[0].name);
-        const { name, files } = event.target;
-        setImage(files[0]);
+    const changeUploadImage = (event) => {
+        const image = event.target.files[0];
+        const name = image.name;
+        console.log(image.name);
         try {
-            const imageRef = ref(storage, `gs://${import.meta.env.VITE_FIREBASE_STORE_BUCKET}/images/${files[0]}`);
+            const imageRef = ref(storage, `gs://${import.meta.env.VITE_FIREBASE_STORE_BUCKET}/images/${name}`);
             uploadBytes(imageRef, image).then(() => {
                 console.log("Uploaded a file!");
+                window.location.href = "/Gallery";
             });
         } catch (err) {
             console.log(err);
         }
     };
-    if (image == null) {
-        return (
-            <div>
-                <Box sx={{ p: 10, border: 'dashed grey' }}>
-                    <Stack direction="row" alignItems="center" spacing={2}>
-                        <Button variant="contained" component="label">
-                            Upload
-                            <input hidden accept="image/*" multiple type="file" onChange={changeUploadImage} />
-                        </Button>
-                    </Stack>
-                </Box>
-            </div>
-        );
-    } else {
-        setTimeout(() => {
-            window.location.href = "/Gallery";
-        }, 3 * 1000);
-        return (
-            <Box sx={{ width: '100%', maxWidth: 500 }}>
-                <Typography variant="h4" gutterBottom>
-                    Galleryに移動します．
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    画像は適切にアップロードされました．
-                </Typography>
-            </Box >
-        );
-    }
+
+    return (
+        <div>
+            <Box sx={{ p: 10, border: 'dashed grey' }}>
+                <Stack direction="row" alignItems="center" spacing={2}>
+                    <Button variant="contained" component="label">
+                        Upload
+                        <input hidden accept="image/*" multiple type="file" onChange={changeUploadImage} />
+                    </Button>
+                </Stack>
+            </Box>
+        </div>
+    );
+
 }
